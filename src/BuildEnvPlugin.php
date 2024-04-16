71145ab7a17ba761fe3ce91b723c76337518a464
ntsiu81 <59295725+ntsiu81@users.noreply.github.com>
2024-04-16 14:07:14 +0200

    Update BuildEnvPlugin.php

--- src/BuildEnvPlugin.php
+++ src/BuildEnvPlugin.php
@@ -12,9 +12,19 @@ class BuildEnvPlugin implements PluginInterface, Capable
     public function activate(Composer $composer, IOInterface $io)
     {
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
     
     public function getCapabilities()
     {
         return ['Composer\Plugin\Capability\CommandProvider' => 'BuildEnv\Composer\BuildEnvProvider'];
     }
 }
+
