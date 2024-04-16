b3e88e9055f82c2a67aec3c6c6f2c05eeb52434d
ntsiu81 <59295725+ntsiu81@users.noreply.github.com>
2024-04-16 18:07:18 +0200

    Update BuildEnvPlugin.php

--- src/BuildEnvPlugin.php
+++ src/BuildEnvPlugin.php
@@ -7,11 +7,19 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Plugin\Capable;
 
-class BuildEnvPlugin implements  Capable
+class BuildEnvPlugin implements PluginInterface, Capable
 {
     public function activate(Composer $composer, IOInterface $io)
     {
     }
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
     
     public function getCapabilities()
     {
